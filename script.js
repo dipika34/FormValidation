81fef1f3c87d9b58ea30f6b4435259aad5f128d9
dipika34 <dipikapaik.10.u.4.2020@gmail.com>
2026-07-06 21:13:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function fill() {
+    temp1 = document.getElementById('input1').value;
+    temp2 = document.getElementById('input2').value;
+    if (temp1 == "123456789" && temp2 == "acbe") {
+        window.alert("Connected Successfully👍");
+    } else {
+        window.alert("Invalid phone number or password");
+    }
+
+
+}
